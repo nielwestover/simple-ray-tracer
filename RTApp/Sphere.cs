fa530a4416155960c11a714c80282bc002c6aaaf
nielwestover@gmail.com <nielwestover@gmail.com@cd0247d9-8874-c182-572d-1225503a987a>
2013-02-07 06:34:05 +0000

    Initial rough code check-in (works).  Time to refactor.

---
+++ RTApp/Sphere.cs
@@ -0,0 +1,34 @@
+// Sphere.cpp: implementation of the Sphere class.
+//
+//////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+class Sphere : Point
+{
+	public double radius;
+
+	public Sphere()
+	{
+
+	}
+
+	public Sphere(double Xc, double Yc, double Zc, double rad, double r, double g, double b)
+	{
+		x = Xc;
+		y = Yc;
+		z = Zc;
+		radius = rad;
+		color.r = r;
+		color.g = g;
+		color.b = b;
+	}
+
+	public Point GetSphereNormal(Point P)
+	{
+		return new Point(0, 0, 0, P.x - x, P.y - y, P.z - z);
+	}
+}
