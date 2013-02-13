cd1cab1ba99c33cfd9f3551b33b77ce71d64b2fa
nielwestover@gmail.com <nielwestover@gmail.com@cd0247d9-8874-c182-572d-1225503a987a>
2013-02-13 19:37:13 +0000

    fix normals finally i think

--- RTApp/Parameters.cs
+++ RTApp/Parameters.cs
@@ -2,31 +2,31 @@
 
 public static class PP
 {
-	public const double XRES = 50;
-	public const double YRES = 50;
+	public const double XRES = 600;
+	public const double YRES = 800;
 
 	public static Point Up = new Point(0, 1, 0);
 	
 	//Cello
-	// 	public static Point At = new Point(-1, .4, -1);
-	// 	public static Point E = new Point(3, 0, 3);
-	// 	public static Point LS = new Point(0, 0, 3);//800,0,-1000,0,0,0);
-	//public const double Lu = 30;
-	//public const double Lv = 300;
+	public static Point At = new Point(0, .3, 0);
+	public static Point E = new Point(2, 0, 2);
+	public static Point LS = new Point(50,0,50);
+	public const double Lu = 3;
+	public const double Lv = 4;
 	
 	//Elf
 // 	public static Point At = new Point(0, 230, 0);
 // 	public static Point E = new Point(5, 230, 90);
 // 	public static Point LS = new Point(50, 230, 100);//800,0,-1000,0,0,0);
 // 	public const double Lu = 100;
-// 	public const double Lv = 150;
+// 	public const double Lv = 75;
 
 	//Test
-	public static Point At = new Point(0, 0, 0);
-	public static Point E = new Point(0, 0, 2);
-	public static Point LS = new Point(0, 0, 2);//800,0,-1000,0,0,0);
-	public const double Lu = 10;
-	public const double Lv = 10;
+	//public static Point At = new Point(0, 0, 0);
+	//public static Point E = new Point(0, 0, 2);
+	//public static Point LS = new Point(0, 0, 2);//800,0,-1000,0,0,0);
+	//public const double Lu = 5;
+	//public const double Lv = 5;
 
 
 	public const double ambient = .1;
--- RTApp/Tri.cs
+++ RTApp/Tri.cs
@@ -48,28 +48,34 @@ class Tri
 
 	public Point triNormal(Point P)
 	{
-		//outputs the normal vector of these 3
+		////outputs the normal vector of these 3
 		//Point normal;
 		//Point P1 = pts[0];
 		//Point P2 = pts[1];
 		//Point P3 = pts[2];
-		//normal = V.cross(V.PDiff(P3, P1), V.PDiff(P2, P1));
+		//normal = V.cross(V.PDiff(P2, P1), V.PDiff(P3, P1));
 
-		//double cos = V.dot(P, normal);
-		//if (cos < 0)
-		//{
-		//    normal.x *= -1;
-		//    normal.y *= -1;
-		//    normal.z *= -1;
-		//}
+		////double cos = V.dot(P, normal);
+		////if (cos < 0)
+		////{
+		////    normal.x *= -1;
+		////    normal.y *= -1;
+		////    normal.z *= -1;
+		////}
+		//return V.normalize(normal);
 
 		Point normal;
 		Point P1 = pts[0];
 		Point P2 = pts[1];
 		Point P3 = pts[2];
-		double t1 = Math.Abs(V.VLength(V.cross(V.PDiff(P, P2), V.PDiff(P, P3))));
-		double t2 = Math.Abs(V.VLength(V.cross(V.PDiff(P, P3), V.PDiff(P, P1))));
-		double t3 = Math.Abs(V.VLength(V.cross(V.PDiff(P, P2), V.PDiff(P, P1))));
+		if (P == P1 || P == P2 || P == P3)
+		{
+			normal = V.cross(V.PDiff(P2, P1), V.PDiff(P3, P1));
+			return normal;
+		}
+		double t1 = Math.Abs(V.VLength(V.cross(V.PDiff(P2, P), V.PDiff(P3, P))));
+		double t2 = Math.Abs(V.VLength(V.cross(V.PDiff(P3, P), V.PDiff(P1, P))));
+		double t3 = Math.Abs(V.VLength(V.cross(V.PDiff(P1, P), V.PDiff(P2, P))));
 		double total = t1 + t2 + t3;
 		double p1 = t1 / total;
 		double p2 = t2 / total;
@@ -79,16 +85,58 @@ class Tri
 		Point N2 = V.vsMult(p2, new Point(P2.nx, P2.ny, P2.nz));
 		Point N3 = V.vsMult(p3, new Point(P3.nx, P3.ny, P3.nz));
 		normal = V.sumVV(N3, V.sumVV(N1, N2));
-		double cos = V.dot(P, normal);
-		if (cos < 0)
-		{
-			normal.x *= -1;
-			normal.y *= -1;
-			normal.z *= -1;
-		}
+		//double cos = V.dot(P, normal);
+		//if (cos < 0)
+		//{
+		//    normal.x *= -1;
+		//    normal.y *= -1;
+		//    normal.z *= -1;
+		//}
 		return V.normalize(normal);
 	}
-
+// 	public Point triNormal2(Point P)
+// 	{
+// 		//outputs the normal vector of these 3
+// // 		Point normal;
+// // 		Point P1 = pts[0];
+// // 		Point P2 = pts[1];
+// // 		Point P3 = pts[2];
+// // 		normal = V.cross(V.PDiff(P2, P1), V.PDiff(P3, P1));
+// // 
+// // 		//double cos = V.dot(P, normal);
+// // 		//if (cos < 0)
+// // 		//{
+// // 		//    normal.x *= -1;
+// // 		//    normal.y *= -1;
+// // 		//    normal.z *= -1;
+// // 		//}
+// // 		return V.normalize(normal);
+// 
+// 		Point normal;
+// 		Point P1 = pts[0];
+// 		Point P2 = pts[1];
+// 		Point P3 = pts[2];
+// 		double t1 = Math.Abs(V.VLength(V.cross(V.PDiff(P2, P), V.PDiff(P3, P))));
+// 		double t2 = Math.Abs(V.VLength(V.cross(V.PDiff(P3, P), V.PDiff(P2, P))));
+// 		double t3 = Math.Abs(V.VLength(V.cross(V.PDiff(P2, P), V.PDiff(P1, P))));
+// 		double total = t1 + t2 + t3;
+// 		double p1 = t1 / total;
+// 		double p2 = t2 / total;
+// 		double p3 = t3 / total;
+// 
+// 		Point N1 = V.vsMult(p1, new Point(P1.nx, P1.ny, P1.nz));
+// 		Point N2 = V.vsMult(p2, new Point(P2.nx, P2.ny, P2.nz));
+// 		Point N3 = V.vsMult(p3, new Point(P3.nx, P3.ny, P3.nz));
+// 		normal = V.sumVV(N3, V.sumVV(N1, N2));
+// 		double cos = V.dot(P, normal);
+// 		if (cos < 0)
+// 		{
+// 			normal.x *= -1;
+// 			normal.y *= -1;
+// 			normal.z *= -1;
+// 		}
+// 		return V.normalize(normal);
+// 	}
 }
 
 
