514f7f78e48aaeac9f8d4438fc01d97bc9dd2351
nielwestover@gmail.com <nielwestover@gmail.com@cd0247d9-8874-c182-572d-1225503a987a>
2013-02-13 05:56:55 +0000

    new normals test

--- trunk/RTApp/Parameters.cs
+++ trunk/RTApp/Parameters.cs
@@ -2,8 +2,8 @@
 
 public static class PP
 {
-	public const double XRES = 3 * 200;
-	public const double YRES = 5 * 200;
+	public const double XRES = 50;
+	public const double YRES = 50;
 
 	public static Point Up = new Point(0, 1, 0);
 	
@@ -11,15 +11,24 @@ public static class PP
 	// 	public static Point At = new Point(-1, .4, -1);
 	// 	public static Point E = new Point(3, 0, 3);
 	// 	public static Point LS = new Point(0, 0, 3);//800,0,-1000,0,0,0);
+	//public const double Lu = 30;
+	//public const double Lv = 300;
 	
 	//Elf
-	public static Point At = new Point(0, 230, 0);
-	public static Point E = new Point(5, 230, 90);
-	public static Point LS = new Point(50, 230, 100);//800,0,-1000,0,0,0);
-	
+// 	public static Point At = new Point(0, 230, 0);
+// 	public static Point E = new Point(5, 230, 90);
+// 	public static Point LS = new Point(50, 230, 100);//800,0,-1000,0,0,0);
+// 	public const double Lu = 100;
+// 	public const double Lv = 150;
 
-	public const double Lu = 100;
-	public const double Lv = 150;
+	//Test
+	public static Point At = new Point(0, 0, 0);
+	public static Point E = new Point(0, 0, 2);
+	public static Point LS = new Point(0, 0, 2);//800,0,-1000,0,0,0);
+	public const double Lu = 10;
+	public const double Lv = 10;
+
+
 	public const double ambient = .1;
 	public const double Pexp = 3;
 }
