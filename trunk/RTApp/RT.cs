a8a7510d53c03138188c55f9ba506db614720485
nielwestover@gmail.com <nielwestover@gmail.com@cd0247d9-8874-c182-572d-1225503a987a>
2013-02-14 07:02:30 +0000

    More cleanup and refactoring.

--- trunk/RTApp/RT.cs
+++ trunk/RTApp/RT.cs
@@ -5,56 +5,59 @@ using System.Text;
 using System.Drawing;
 using System.Diagnostics;
 
+//Reflection vector
+//R = I - 2(N ● I)N
+
 namespace RTApp
 {
 	public class RT
 	{
-		List<Sphere> spheres = new List<Sphere>();
-		List<Tri> tris = new List<Tri>();
+		List<Shape> shapes = new List<Shape>();
 		Bitmap bitmap = new Bitmap((int)PP.XRES, (int)PP.YRES);
 
 		bool obstructionBetween(Point A, Point ColorMe)
 		{
 			Point Ray = V.normalize(V.PDiff(A, ColorMe));
-			for (int k = 0; k < spheres.Count; k++)
+			for (int k = 0; k < shapes.Count; k++)
 			{
-				double a = Ray.x * Ray.x + Ray.y * Ray.y + Ray.z * Ray.z;
-				double b = 2 * ((ColorMe.x - spheres[k].center.x) * Ray.x + (ColorMe.y - spheres[k].center.y) * Ray.y + (ColorMe.z - spheres[k].center.z) * Ray.z);
-				double c = ((ColorMe.x - spheres[k].center.x) * (ColorMe.x - spheres[k].center.x) + (ColorMe.y - spheres[k].center.y) * (ColorMe.y - spheres[k].center.y) + (ColorMe.z - spheres[k].center.z) * (ColorMe.z - spheres[k].center.z) - spheres[k].radius * spheres[k].radius);
-				if (b * b > 4 * a * c)
+				if (shapes[k] is Sphere)
 				{
-					double squareRt = Math.Sqrt(b * b - 4 * a * c);
-					double T1 = (-b + squareRt) / (2 * a);
-					double T2 = (-b - squareRt) / (2 * a);
+					Sphere s = shapes[k] as Sphere;
+					double a = Ray.x * Ray.x + Ray.y * Ray.y + Ray.z * Ray.z;
+					double b = 2 * ((ColorMe.x - s.center.x) * Ray.x + (ColorMe.y - s.center.y) * Ray.y + (ColorMe.z - s.center.z) * Ray.z);
+					double c = ((ColorMe.x - s.center.x) * (ColorMe.x - s.center.x) + (ColorMe.y - s.center.y) * (ColorMe.y - s.center.y) + (ColorMe.z - s.center.z) * (ColorMe.z - s.center.z) - s.radius * s.radius);
+					if (b * b > 4 * a * c)
+					{
+						double squareRt = Math.Sqrt(b * b - 4 * a * c);
+						double T1 = (-b + squareRt) / (2 * a);
+						double T2 = (-b - squareRt) / (2 * a);
 
-					if (T1 > 0.1 || T2 > 0.1)
+						if (T1 > 0.0001 || T2 > 0.0001)
+						{
+							return true;
+						}
+					}
+				}
+				if (shapes[k] is Tri)
+				{
+					Tri tri = shapes[k] as Tri;
+					Point P1 = tri.pts[0];
+					Point P2 = tri.pts[1];
+					Point P3 = tri.pts[2];
+					Point N = tri.triNormal(P1);
+					//N.Print();
+					double D = -(N.x * P1.x) - (N.y * P1.y) - (N.z * P1.z);
+					double T1 = V.dot(N, V.PDiff(P1, ColorMe)) / V.dot(N, Ray);
+					if (T1 < .0001)
+						continue;
+					Point P = V.sumPV(ColorMe, V.vsMult(T1, Ray));
+
+					Tri.UVW bc = tri.getBarycentricCoordsAtPoint(P);
+					if ((bc.v >= 0) && (bc.w >= 0) && (bc.v + bc.w < 1.0))
 					{
 						return true;
 					}
 				}
-
-			}
-			for (int k = 0; k < tris.Count; k++)
-			{
-				Point Aa = tris[k].pts[0];
-				Point B = tris[k].pts[1];
-				Point C = tris[k].pts[2];
-				Point N = tris[k].triNormal(Aa);
-				//N.Print();
-				double D = -(N.x * Aa.x) - (N.y * Aa.y) - (N.z * Aa.z);
-				double T2 = -(N.x * PP.E.x + N.y * PP.E.y + N.z * PP.E.z + D) / (N.x * Ray.x + N.y * Ray.y + N.z * Ray.z);
-
-				double T1 = V.dot(N, V.PDiff(Aa, ColorMe)) / V.dot(N, Ray);
-				if (T1 < .0001)
-					continue;
-				Point P = V.sumPV(ColorMe, V.vsMult(T1, Ray));
-
-				Tri.UVW bc = tris[k].getBarycentricCoordsAtPoint(P);
-				if ((bc.v >= 0) && (bc.w >= 0) && (bc.v + bc.w < 1.0))
-				{
-					return true;
-				}
-
 
 			}
 			return false;
@@ -62,67 +65,29 @@ namespace RTApp
 
 		RGB getColorAtPoint(int i, int j, Point P, Point N, int SI)
 		{
-			//cout<<endl<<"P: ";
-			//P.Print();
-			//cout<<"N: ";
-			//N.Print();
 			Point L = V.normalize(V.PDiff(PP.LS, P));
-			//cout<<"L: ";
-			//L.Print();
 			double LdotN = V.dot(L, N);
-			//cout<<"LDOTN: "<<LdotN<<endl;
 			Point ACC = V.vsMult(2 * LdotN, N);
 			Point R = new Point(ACC.x - L.x, ACC.y - L.y, ACC.z - L.z);
-			//cout<<"R: ";
-			//R.Print();
 			double Ca = PP.ambient + (1 - PP.ambient) * LdotN;
-			//cout<<"Ca: "<<Ca<<endl;
 			double ACC2 = V.dot(V.normalize(R), V.normalize(V.PDiff(PP.E, P)));
-			//cout<<"ACC2: "<<ACC2<<endl;
 			double Cs = Math.Pow(ACC2, PP.Pexp);
-			//cout<<"Cs: "<<Cs<<endl;
-			double newR, newG, newB;
-			if (SI < 10)
-			{
-				newR = (Ca * spheres[SI].color.r + (1 - Ca * spheres[SI].color.r) * Cs);
-				newG = (Ca * spheres[SI].color.g + (1 - Ca * spheres[SI].color.g) * Cs);
-				newB = (Ca * spheres[SI].color.b + (1 - Ca * spheres[SI].color.b) * Cs);
-			}
-			else
-			{
-				newR = (Ca * tris[SI - 10].color.r + (1 - Ca * tris[SI - 10].color.r) * Cs);
-				newG = (Ca * tris[SI - 10].color.g + (1 - Ca * tris[SI - 10].color.g) * Cs);
-				newB = (Ca * tris[SI - 10].color.b + (1 - Ca * tris[SI - 10].color.b) * Cs);
-			}
-
+			
+			double newR = (Ca * shapes[SI].color.r + (1 - Ca * shapes[SI].color.r) * Cs);
+			double newG = (Ca * shapes[SI].color.g + (1 - Ca * shapes[SI].color.g) * Cs);
+			double newB = (Ca * shapes[SI].color.b + (1 - Ca * shapes[SI].color.b) * Cs);
+			
 			return new RGB(newR, newG, newB);
 
 		}
 		RGB getAmbientColor(int i, int j, Point P, Point N, int SI)
 		{
-			//cout<<endl<<"P: ";
-			//P.Print();
-			//cout<<"N: ";
-			//N.Print();
 			Point L = V.normalize(V.PDiff(PP.LS, P));
-			//cout<<"L: ";
-			//L.Print();
 			double LdotN = V.dot(L, N);
-			//cout<<"LDOTN: "<<LdotN<<endl;
 			double Ca = PP.ambient + (1 - PP.ambient) * LdotN;
-			double newR, newG, newB;
-			if (SI < 10)
-			{
-				newR = PP.ambient * spheres[SI].color.r;
-				newG = PP.ambient * spheres[SI].color.g;
-				newB = PP.ambient * spheres[SI].color.b;
-			}
-			else
-			{
-				newR = PP.ambient * tris[SI - 10].color.r;
-				newG = PP.ambient * tris[SI - 10].color.g;
-				newB = PP.ambient * tris[SI - 10].color.b;
-			}
+			double newR = Ca * shapes[SI].color.r;
+			double newG = Ca * shapes[SI].color.g;
+			double newB = Ca * shapes[SI].color.b;
 			return new RGB(newR, newG, newB);
 		}
 
@@ -187,10 +152,10 @@ namespace RTApp
 				Point a = new Point(md.Vertices[item.P1.Vertex], md.TexCoords[item.P1.TexCoord], md.Normals[item.P1.Normal]);
 				Point b = new Point(md.Vertices[item.P2.Vertex], md.TexCoords[item.P2.TexCoord], md.Normals[item.P2.Normal]);
 				Point c = new Point(md.Vertices[item.P3.Vertex], md.TexCoords[item.P3.TexCoord], md.Normals[item.P3.Normal]);
-				tris.Add(new Tri(a, b, c, new RGB(1, .4, .2)));
+				shapes.Add(new Tri(a, b, c, new RGB(1, .4, .2)));
 			}
 
-			//spheres.Add(new Sphere(0, 0, -5, 4, 1, 0, 0));
+			shapes.Add(new Sphere(70, 230, 30, 20, 1, 1, 0));
 			////spheres[1] = new Sphere(3, 5, -5, 2, 0, 1, 0);
 			////spheres[1] = new Sphere(-4, 10, 0, 1, 0, 1, 0);
 			////spheres[2] = new Sphere(10, 10, 0, 3, 0, 0, 1);
@@ -233,50 +198,37 @@ namespace RTApp
 			double T = double.PositiveInfinity;
 
 			//calculate T for each sphere
-			for (int k = 0; k < spheres.Count; k++)
+			for (int k = 0; k < shapes.Count; k++)
 			{
-				double a = Ray.x * Ray.x + Ray.y * Ray.y + Ray.z * Ray.z;
-				double b = 2 * ((PP.E.x - spheres[k].center.x) * Ray.x + (PP.E.y - spheres[k].center.y) * Ray.y + (PP.E.z - spheres[k].center.z) * Ray.z);
-				double c = ((PP.E.x - spheres[k].center.x) * (PP.E.x - spheres[k].center.x) + (PP.E.y - spheres[k].center.y) * (PP.E.y - spheres[k].center.y) + (PP.E.z - spheres[k].center.z) * (PP.E.z - spheres[k].center.z) - spheres[k].radius * spheres[k].radius);
-				
-				if (b * b > 4 * a * c)
+				if (shapes[k] is Sphere)
 				{
-					double squareRt = Math.Sqrt(b * b - 4 * a * c);
-					double T1 = (-b + squareRt) / (2 * a);
-					double T2 = (-b - squareRt) / (2 * a);
 
-					if (T1 > 0 && T1 <= T2 && T1 < T)
+					double newT = shapes[k].IntersectDistance(Ray);
+					if (newT != double.PositiveInfinity)
 					{
-						T = T1;
-						SI = k;
-					}
-					else if (T2 > 0 && T2 <= T1 && T2 < T)
-					{
-						T = T2;
+						T = newT;
 						SI = k;
 					}
 				}
-			}
-
-			//compute T for Triangles as well here, find lowest T value
-			for (int k = 0; k < tris.Count; k++)
-			{
-				Point A = tris[k].pts[0];
-				Point B = tris[k].pts[1];
-				Point C = tris[k].pts[2];
-
-				Point N = tris[k].triNormal(A);
-				double newT = V.dot(N, V.PDiff(A, PP.E)) / V.dot(N, Ray);
-				Point P = V.sumPV(PP.E, V.vsMult(newT, Ray));
-
-				Tri.UVW bc = tris[k].getBarycentricCoordsAtPoint(P);
-				if ((bc.v >= 0) && (bc.w >= 0) && (bc.v + bc.w < 1.0))
+				if (shapes[k] is Tri)
 				{
-					//Closer than old T? Update T and index
-					if (newT < T)
+					//Calc T
+					double newT = shapes[k].IntersectDistance(Ray);
+					//Find intersection point at distance T
+					Point P = V.sumPV(PP.E, V.vsMult(newT, Ray));
+
+					//Get barycentric coords for intersect point
+					Tri.UVW bc = (shapes[k] as Tri).getBarycentricCoordsAtPoint(P);
+
+					//See if intersect point is inside triangle
+					if ((bc.v >= 0) && (bc.w >= 0) && (bc.v + bc.w < 1.0))
 					{
-						T = newT;
-						SI = k + 10;
+						//Closer than old T? Update T and index
+						if (newT < T)
+						{
+							T = newT;
+							SI = k;
+						}
 					}
 				}
 			}
@@ -286,10 +238,10 @@ namespace RTApp
 				Point ColorMe = V.sumPV(PP.E, V.vsMult(T, Ray));
 				bool obs = obstructionBetween(PP.LS, ColorMe);
 				Point N;
-				if (SI < 10)
-					N = V.normalize(spheres[SI].GetSphereNormal(ColorMe));
+				if (shapes[SI] is Sphere)
+					N = V.normalize((shapes[SI] as Sphere).GetSphereNormal(ColorMe));
 				else
-					N = tris[SI - 10].triNormal(ColorMe);
+					N = (shapes[SI] as Tri).triNormal(ColorMe);
 				if (!obs)
 					colorAtPixel = getColorAtPoint(i, j, ColorMe, N, SI);
 				else
--- trunk/RTApp/Tri.cs
+++ trunk/RTApp/Tri.cs
@@ -3,12 +3,12 @@
 //////////////////////////////////////////////////////////////////////
 
 using System;
-class Tri
+using RTApp;
+class Tri : Shape
 {
 
 	public int numPoints;
 	public Point[] pts = new Point[3];
-	public RGB color;
 
 	//cache these - the same every time we do barycentric coords
 	Point v0;
@@ -62,29 +62,13 @@ class Tri
 
 	public Point triNormal(Point P)
 	{
-		////outputs the normal vector of these 3
-		//Point normal;
-		//Point P1 = pts[0];
-		//Point P2 = pts[1];
-		//Point P3 = pts[2];
-		//normal = V.cross(V.PDiff(P2, P1), V.PDiff(P3, P1));
-
-		////double cos = V.dot(P, normal);
-		////if (cos < 0)
-		////{
-		////    normal.x *= -1;
-		////    normal.y *= -1;
-		////    normal.z *= -1;
-		////}
-		//return V.normalize(normal);
-
 		Point normal;
 		Point P1 = pts[0];
 		Point P2 = pts[1];
 		Point P3 = pts[2];
 		if (P == P1 || P == P2 || P == P3)
 		{
-			normal = V.cross(V.PDiff(P2, P1), V.PDiff(P3, P1));
+			normal = V.normalize(V.cross(V.PDiff(P2, P1), V.PDiff(P3, P1)));
 			return normal;
 		}
 		UVW bc = getBarycentricCoordsAtPoint(P);
@@ -137,49 +121,17 @@ class Tri
 
 		return new UVW {u = uC, v=vC, w=wC };
 	}
-// 	public Point triNormal2(Point P)
-// 	{
-// 		//outputs the normal vector of these 3
-// // 		Point normal;
-// // 		Point P1 = pts[0];
-// // 		Point P2 = pts[1];
-// // 		Point P3 = pts[2];
-// // 		normal = V.cross(V.PDiff(P2, P1), V.PDiff(P3, P1));
-// // 
-// // 		//double cos = V.dot(P, normal);
-// // 		//if (cos < 0)
-// // 		//{
-// // 		//    normal.x *= -1;
-// // 		//    normal.y *= -1;
-// // 		//    normal.z *= -1;
-// // 		//}
-// // 		return V.normalize(normal);
-// 
-// 		Point normal;
-// 		Point P1 = pts[0];
-// 		Point P2 = pts[1];
-// 		Point P3 = pts[2];
-// 		double t1 = Math.Abs(V.VLength(V.cross(V.PDiff(P2, P), V.PDiff(P3, P))));
-// 		double t2 = Math.Abs(V.VLength(V.cross(V.PDiff(P3, P), V.PDiff(P2, P))));
-// 		double t3 = Math.Abs(V.VLength(V.cross(V.PDiff(P2, P), V.PDiff(P1, P))));
-// 		double total = t1 + t2 + t3;
-// 		double p1 = t1 / total;
-// 		double p2 = t2 / total;
-// 		double p3 = t3 / total;
-// 
-// 		Point N1 = V.vsMult(p1, new Point(P1.nx, P1.ny, P1.nz));
-// 		Point N2 = V.vsMult(p2, new Point(P2.nx, P2.ny, P2.nz));
-// 		Point N3 = V.vsMult(p3, new Point(P3.nx, P3.ny, P3.nz));
-// 		normal = V.sumVV(N3, V.sumVV(N1, N2));
-// 		double cos = V.dot(P, normal);
-// 		if (cos < 0)
-// 		{
-// 			normal.x *= -1;
-// 			normal.y *= -1;
-// 			normal.z *= -1;
-// 		}
-// 		return V.normalize(normal);
-// 	}
+
+	public override double IntersectDistance(Point Ray)
+	{
+		Point A = pts[0];
+		Point B = pts[1];
+		Point C = pts[2];
+
+		Point N = triNormal(A);
+		double T = V.dot(N, V.PDiff(A, PP.E)) / V.dot(N, Ray);
+		return T;
+	} 
 }
 
 
